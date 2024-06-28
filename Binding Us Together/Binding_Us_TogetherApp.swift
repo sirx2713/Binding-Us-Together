a79f2f61e1f8eaa047da57989a2a952c6e417333
iSAD <bradx2713@gmail.com>
2024-06-28 11:26:07 +0200

    Initial Commit

---
+++ Binding Us Together/Binding_Us_TogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Binding_Us_TogetherApp.swift
+//  Binding Us Together
+//
+//  Created by Tafadzwa Alexander on 2024/06/28.
+//
+
+import SwiftUI
+
+@main
+struct Binding_Us_TogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binding Us Together/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Binding Us Together
+//
+//  Created by Tafadzwa Alexander on 2024/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
