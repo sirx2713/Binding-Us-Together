0ad2d3d7dce41beda3fd8ce7f723a8cc0c266677
iSAD <bradx2713@gmail.com>
2024-06-28 11:49:50 +0200

    Working on the idea!

---
+++ Binding Us Together/secondView.swift
@@ -0,0 +1,18 @@
+//
+//  secondView.swift
+//  Binding Us Together
+//
+//  Created by Tafadzwa Alexander on 2024/06/28.
+//
+
+import SwiftUI
+
+struct secondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    secondView()
+}
